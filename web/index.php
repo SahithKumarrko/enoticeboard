cb036bccab8501e1e49438efed2b88e7f2451733
SahithKumarrko <36582308+SahithKumarrko@users.noreply.github.com>
2019-09-27 14:38:06 +0530

    Create index.php

---
+++ web/index.php
@@ -0,0 +1,6 @@
+<?php
+    $db_connection = pg_connect("host=ec2-50-19-222-129.compute-1.amazonaws.com port=5432 dbname=dd453fahinu1bq user=fpslozyngkhrbf password=6188eb03be51c3ae85a7d96d1c6bc7e91a0006682cd7bd692f39f263d5b6e8a3");
+	echo $db_connection;
+	
+	echo "hii";
+?>
